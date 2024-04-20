bfcf041724467fa5d5ff52823a3331db73ecd0ff
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2024-04-20 16:56:11 +0530

    Update 5SwapNumbersWIthout3rdVariable.py

--- 5SwapNumbersWIthout3rdVariable.py
+++ 5SwapNumbersWIthout3rdVariable.py
@@ -1,1 +1,8 @@
-
+#swap two numbers without using 3rd variable
+a=int(input("Give 1st number:"))
+b=int(input("Give 2nt number:"))
+print("1st and 2nd number before swapping is ",a," ",b)
+a=a+b
+b=a-b
+a=a-b
+print("1st and 2nd number after swapping is ",a," ",b)
