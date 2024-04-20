21699c8dc052bd7199e4ee3886da2f4ac3baf937
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2024-04-20 17:02:06 +0530

    Update 6RandomNumber.py

--- 6RandomNumber.py
+++ 6RandomNumber.py
@@ -1,1 +1,6 @@
-
+#Random number generation between range
+a=int(input("Give starting range:"))
+b=int(input("GIve ending range"))
+import random
+num=random.randint(a,b)
+print("The generated random number is:",num)
