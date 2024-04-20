f7f3746af532c662bd83d952ce24704cb85ba3ad
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2024-04-20 16:37:13 +0530

    Update 11QuadraticEquation.py

--- 11QuadraticEquation.py
+++ 11QuadraticEquation.py
@@ -1,1 +1,9 @@
-
+#ax**2+bx+c
+import cmath
+a=int(input("Give a (a!=0):"))
+b=int(input("Give b:"))
+c=int(input("Give c:"))
+det=b**2-4*a*c
+r1=(-b+cmath.sqrt(det))/(2*a)
+r2=(-b-cmath.sqrt(det))/(2*a)
+print("Root 1 is ",r1," and root 2 is ",r2)
