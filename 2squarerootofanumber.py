e44ccba8ab704f5754d68d870798bfdd72bc81a4
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2024-04-20 16:13:27 +0530

    Update 2squarerootofanumber.py

--- 2squarerootofanumber.py
+++ 2squarerootofanumber.py
@@ -1,3 +1,15 @@
+Method 1:
+This method is not using any inbuilt python function like math
 num=int(input("GIve the number:"))
 squareroot=num**(1/2)
 print("Square root of ",num," is ",squareroot)
+
+
+
+
+Method 2:
+This method uses the inbuilt math function by importing it as import math
+import math
+a=int(input("GIve number:"))
+p=math.sqrt(a)
+print("The square root of ",a," is",p)
