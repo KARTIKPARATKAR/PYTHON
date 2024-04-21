6f0641e8082aa137f5339fc40faab3d247ec75d6
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2024-04-21 10:12:56 +0530

    Create 11RangefninForloop.py

---
+++ 11RangefninForloop.py
@@ -0,0 +1,11 @@
+#1)Range function in python
+for i in range(0,10,2):  #Here 0 is the starting and 10 is the ending of range without 10. And 2 indicats that value of i will be increamented by 2 after each iteration
+  print(i)
+
+
+#2)
+for i in range(10,0,-2):  #Here 10 is the starting and 0 is the ending of range without 0. And -2 indicats that value of i will be decreamented by 2 after each iteration
+  print(i)
+
+
+#1st program will output as 0,2,4,6,8 and 2nd program will give output as 10,8,6,4,2
