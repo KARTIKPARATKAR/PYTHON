cabe29d666d8a2b1507eaa4a0c7156e9134dbdca
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2024-04-21 22:53:48 +0530

    Create 19NumberSystemConversion.py

---
+++ 19NumberSystemConversion.py
@@ -0,0 +1,5 @@
+#Conversion of decimal to binary,octal and hexdecimal
+decimal=int(input("Give the decimal number:"))
+print("Binary of ",decimal," is ",bin(decimal))
+print("Octal of ",decimal," is ",oct(decimal))
+print("Hexadecimal of ",decimal," is ",hex(decimal))
