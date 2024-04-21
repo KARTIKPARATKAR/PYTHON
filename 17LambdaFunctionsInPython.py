252366f13fc791b1d0f2e1b26fdb8b16a440e0ba
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2024-04-21 20:19:03 +0530

    Create 17LambdaFunctionsInPython.py

---
+++ 17LambdaFunctionsInPython.py
@@ -0,0 +1,14 @@
+#Lambda functions 
+sum=lambda a,b:(a+b)/2   #This is a lambda function of sum 
+print(sum(2,3))  
+
+
+
+#This is a lambda function of cube
+cube=lambda a:a*a*a
+print(cube(3))
+
+
+#This is a function for printing "Hello Wold"
+printthis=lambda true:print("Hello World")
+print(printthis(4))
