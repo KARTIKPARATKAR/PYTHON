69fb4f3bde84ee890452f7a6ca7af82b42ba7495
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2024-04-21 19:25:31 +0530

    Update 16SumOfNaturalNumbersBetweenTwoIntegers.py

--- 16SumOfNaturalNumbersBetweenTwoIntegers.py
+++ 16SumOfNaturalNumbersBetweenTwoIntegers.py
@@ -1,3 +1,4 @@
+#THis is method 1 using the formula of finding the sum of first n natural numbers.
 #Sum of natural numbers
 a=int(input("Give the starting number"))
 b=int(input("Give the ending number"))
@@ -7,3 +8,13 @@ sum2=(b*(b+1))//2
 print("Sum of ending:",sum2)
 sum=sum2-sum1
 print("Sum of numbers between ",a," and ",b," is",sum)
+
+
+#This is a method using the for loop which iterate through the given range and add all the numbers in variable sum to get the desired sum
+#Sum of natural numbers
+a=int(input("Give the starting number"))
+b=int(input("Give the ending number"))
+sum=0
+for i in range(a,b):
+    sum=sum+i
+print("The sum between ",a," and ",b," is ",sum)
