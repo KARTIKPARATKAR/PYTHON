2c41ed14423c850dac2d0e452688edbe4d22f08e
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2024-04-21 23:04:34 +0530

    Create 20ASCIIValuesOfCharacter.py

---
+++ 20ASCIIValuesOfCharacter.py
@@ -0,0 +1,4 @@
+#Convert the special charater symbols in equivalent ASCII(American STandard Code Of Information INterchange)
+p=input("Give the input character")
+for i in p:
+    print("ASCII of ",i," is ",ord(i))
