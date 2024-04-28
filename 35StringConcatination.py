2e2ee3cf6259624bce7e027c7cb864cb3e845495
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2024-04-28 19:48:59 +0530

    Create 35StringConcatination.py

---
+++ 35StringConcatination.py
@@ -0,0 +1,9 @@
+#String concatination
+x=input("Give 1st string:")
+y=input("Give 2nd string:")
+z=x+" "+y
+print("Strings after concatination is:",z)
+
+p=22
+test="His age is "+ str(p)
+print(test)
