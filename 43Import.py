cd2e0f55372ae2d989b9281b772cc39ffebc8c51
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2025-02-11 09:50:13 +0530

    Create 43Import.py

---
+++ 43Import.py
@@ -0,0 +1,8 @@
+#Importing in python is the process of loading code from a python module into current script.
+import math
+p=math.sqrt(16)
+print(p)
+print(dir(math))   #This will print all the functions that can be used when you import math built in
+print(type(math.pi))   #This will print type of build in "pi"
+
+import tensorflow as tf  #Using as keyword,u dont need to use tensorflow word everytime. Instead you can use tf 
