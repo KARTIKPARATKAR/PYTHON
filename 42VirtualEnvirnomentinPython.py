b6cb4d01a0e441f185487bf8b33442fe42cd2e4a
KARTIK SANJAY PARATKAR <100400207+KARTIKPARATKAR@users.noreply.github.com>
2025-02-11 09:36:36 +0530

    Create 42VirtualEnvirnomentinPython.py

---
+++ 42VirtualEnvirnomentinPython.py
@@ -0,0 +1,17 @@
+#A Python Virtual Environment is an isolated space where you can work on your Python projects,
+#separately from your system-installed Python.
+#A virtual environment is a tool that helps to keep dependencies required by different projects 
+#separate by creating isolated Python virtual environments for them. This is one of the most
+#important tools that most Python developers use.
+
+
+#Create a virtual evnironment by following command:
+python -m venv myenv
+
+
+#Activate the virtual environment by following cmd command:
+myenv\Scripts\activate
+
+
+#For deactivating virtual environment , use following command:
+deactivate
